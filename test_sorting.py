57323e568136c5c530d435587ffc1aefbc508daa
Eden.Kellner <ednkell@gmail.com>
2023-05-20 20:30:58 +0300

    insertion sort tested, test simplified

--- test_sorting.py
+++ test_sorting.py
@@ -1,8 +1,8 @@
 import pytest
-import os
-import importlib
+from sorts.insertion_sort import sort as insertion_sort
+from sorts.selection_sort import sort as selection_sort
 
-def test_sorting_algorithm(sort_func):
+def _test_sorting_algorithm(sort_func):
     # Test case 1: Array in ascending order
     A = [1, 2, 3, 4, 5]
     assert sort_func(A) == sorted(A)
@@ -27,26 +27,8 @@ def test_sorting_algorithm(sort_func):
     A = [-5, 0, 3, -2, 1]
     assert sort_func(A) == sorted(A)
 
-def pytest_generate_tests(metafunc):
-    if "sort_func" in metafunc.fixturenames:
-        # Get the path of the "sorts" directory
-        sorts_dir = os.path.dirname(os.path.abspath(__file__)) + '/sorts'
+def test_selection_sort():
+    _test_sorting_algorithm(selection_sort)
 
-        # Iterate over each file in the "sorts" directory
-        for file_name in os.listdir(sorts_dir):
-            if file_name.endswith('.py') and file_name != '__init__.py':
-                # Remove the file extension to get the module name
-                module_name = file_name[:-3]
-
-                # Dynamically import the sorting algorithm module
-                sort_module = importlib.import_module(f'sorts.{module_name}')
-
-                # Check if the specified sorting algorithm function exists
-                if hasattr(sort_module, 'sort'):
-                    # Retrieve the sorting algorithm function
-                    sort_func = getattr(sort_module, 'sort')
-
-                    # Add the sorting algorithm function as a parameter to the test function
-                    metafunc.parametrize("sort_func", [sort_func], ids=[module_name])
-                else:
-                    pytest.fail(f"Sorting algorithm 'sort' not found in module '{module_name}'")
+def test_insertion_sort():
+    _test_sorting_algorithm(insertion_sort)
