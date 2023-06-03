825c9e5d123fda05261e31c2cc7d1ab419da892b
Eden.Kellner <ednkell@gmail.com>
2023-06-03 12:18:24 +0300

    Binary seach recursive solution added as well

--- searches/binary_search.py
+++ searches/binary_search.py
@@ -4,7 +4,7 @@ def search(arr: List[int], value: int) -> Optional[int]:
     return _binary_search_iter(arr, 0, len(arr), value)
 
 def _binary_search_iter(arr: List[int], l: int, r: int, value: int) -> Optional[int]:
-    while l + 1 <= r:
+    while l < r:
         m = (l + r) // 2 # floor (l + r) / 2
         if arr[m] < value:
             l = m + 1
@@ -13,3 +13,14 @@ def _binary_search_iter(arr: List[int], l: int, r: int, value: int) -> Optional[
         else:
             return m
     return None
+
+def _binary_search_rec(arr:List[int], l: int, r: int, value: int) -> Optional[int]:
+    if l >= r:
+        return None
+    m = (l + r) // 2 # floor (l + r) / 2
+    if arr[m] < value:
+        return _binary_search_rec(arr, m + 1, r, value)
+    elif arr[m] > value:
+        return _binary_search_rec(arr, l, m - 1, value)
+    else:
+        return m
