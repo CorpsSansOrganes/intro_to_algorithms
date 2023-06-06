484e43a0fe582dc72a7f4cd9d5e3f555eb1872d9
Eden.Kellner <ednkell@gmail.com>
2023-06-06 17:12:45 +0300

    counting sort impl

--- sorts/counting_sort.py
+++ sorts/counting_sort.py
@@ -1,6 +1,18 @@
 from typing import List 
+from itertools import accumulate
 def sort(arr: List[int]) -> List[int]:
     return counting_sort(arr, max(arr))
 
 def counting_sort(arr: List[int], k: int) -> List[int]:
-    return []
+    res = [0] * len(arr)
+    count = [0] * k 
+
+    for a in arr:
+        count[a] += 1
+    count = list(accumulate(count))
+    
+    for a in reversed(arr):
+        res[count[a]] = a 
+        count[a] -= 1
+    
+    return res
