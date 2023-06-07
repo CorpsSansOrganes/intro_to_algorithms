41b751cf4e87b66ff63116c93b4d9aa5c3ca2090
Eden.Kellner <ednkell@gmail.com>
2023-06-07 13:33:46 +0300

    radix sort init

--- sorts/counting_sort.py
+++ sorts/counting_sort.py
@@ -1,5 +1,6 @@
 from typing import List 
 from itertools import accumulate
+
 def sort(arr: List[int]) -> List[int]:
     if not arr:
         return arr
---
+++ sorts/radix_sort.py
@@ -0,0 +1,8 @@
+from typing import List
+
+def sort(arr: List[int]) -> List[int]:
+    d = len(str(max(arr)))
+    radix_sort(arr, d)
+
+def radix_sort(arr: List[int], d: int) -> List[int]:
+    return []
